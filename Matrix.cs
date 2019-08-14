5807967366e29629739ed4863be9bcf84486517d
Pieter Boots <Pieterboots@zonnet.nl>
2019-08-14 21:21:42 +0200

    Update Matrix.cs

--- Matrix.cs
+++ Matrix.cs
@@ -24,6 +24,118 @@ class Matrix
             Values[x] = InitValue;
         }
     }
+    
+    //----------------------------------
+
+    public static Matrix GetMatrixRandom(int _N,Random rnd)
+    {
+        Matrix M=new Matrix(_N);
+     
+           for (int x = 0; x < M.N * M.N; x++)
+        {
+            M.Values[x] = rnd.Next(256)-128;
+        }
+        M.Normalize();
+        return M;
+    }
+
+    //----------------------------------
+
+    public static Matrix[] GetMatrixArrayRandom(int _N, Int32 cnt,Random rnd)
+    {
+        Matrix[] coefs = new Matrix[cnt];      
+        for (int i = 0; i < cnt; i++)
+        {
+            coefs[i] = Matrix.GetMatrixRandom(_N,rnd);
+        }
+        return coefs;
+    }
+
+    //----------------------------------
+
+    public void  MatrixRandom()
+    {        
+        Random rnd = new Random();
+        for (int x = 0; x < N* N; x++)
+        {
+            Values[x] = rnd.Next(256) - 128;
+        }
+        Normalize();
+       
+    }
+
+    //----------------------------------
+
+    public static Matrix[] GetMatrixArrayEmpty(int _N, Int32 cnt)
+    {
+        Matrix[] coefs = new Matrix[cnt];
+        for (int i = 0; i < cnt; i++)
+        {
+            coefs[i] = new Matrix(_N, 0);
+        }
+        return coefs;
+    }
+    //----------------------------------
+
+    public  Boolean AllZero()
+    {
+       
+        for (int i = 0; i < N; i++)
+        {
+            if (Values[i] != 0 )
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //----------------------------------
+
+    public void Fill(Double Value)
+    {                 
+        for (int x = 0; x < N * N; x++)
+        {
+            Values[x] = Value;
+        }       
+    }
+
+    //----------------------------------
+
+    public void FillRnd(Random rnd)
+    {     
+        for (int x = 0; x < N * N; x++)
+        {
+            Values[x] = rnd.Next(256) - 128;
+        }
+        Normalize();      
+    }
+
+    //----------------------------------
+
+    public void FillRnd(Random rnd,int[,] Example)
+    {
+        int x = rnd.Next(Example.GetLength(0)-N);
+        int y = rnd.Next(Example.GetLength(1) - N);
+        for (int y1 = 0; y1 < N ; y1++)
+        {
+            for (int x1 = 0; x1 < N ; x1++)
+            {
+                Values[x1+y1*N] = Example[x1+x,y1+y];
+            }
+        }
+        Normalize();
+    }
+
+    //----------------------------------
+
+    public void CopyFrom(Matrix M)
+    {
+        for (int x = 0; x < N * N; x++)
+        {
+            Values[x] = M.Values[x];
+        }
+    }
 
     //----------------------------------
 
@@ -50,7 +162,7 @@ class Matrix
         for (int x = 0; x < a1.N * a1.N; x++)
         {
             Value = Value + a1.Values[x] * b1.Values[x];
-        }
+        } 
         return Value;
     }
 
@@ -269,6 +381,7 @@ class Matrix
         }
         avg = total / (N * N);
         double rangetotal = 0;
+   
         for (int x = 0; x < N * N; x++)
         {
             rangetotal = rangetotal + (Values[x] - avg) * (Values[x] - avg);
@@ -278,6 +391,31 @@ class Matrix
         {
             Values[x] = (Values[x] - avg) / maxrange;
         }
+    }
+
+    //----------------------------------
+
+    public Matrix GetNormalized()
+    {
+        Matrix M = new Matrix(N);
+        double avg = 0;
+        double total = 0;
+        for (int x = 0; x < N * N; x++)
+        {
+            total = total + Values[x];
+        }
+        avg = total / (N * N);
+        double rangetotal = 0;
+        for (int x = 0; x < N * N; x++)
+        {
+            rangetotal = rangetotal + (Values[x] - avg) * (Values[x] - avg);
+        }
+        double maxrange = Math.Sqrt(rangetotal);
+        for (int x = 0; x < N * N; x++)
+        {
+           M.Values[x] = (Values[x] - avg) / maxrange;
+        }
+        return M;
     }
 }
 
